82933fb1c2c13277f35f6d060e0c831b1eb6750c
Melissa Spalione <melissaspalione86@gmail.com>
2019-09-07 12:41:00 -0400

    Time display

--- BeginToCodeWithCSharp/Chapters/Chapter 04/Ch04_06_TimeDisplay.cs
+++ BeginToCodeWithCSharp/Chapters/Chapter 04/Ch04_06_TimeDisplay.cs
@@ -1,12 +1,23 @@
 ﻿using SnapsLibrary;
 
+
 class Ch04_06_TimeDisplay
 {
     public void StartProgram()
     {
         SnapsEngine.SetTitleString("Current Time");
-        int hourValue = SnapsEngine.GetHourValue();
-        int minuteValue = SnapsEngine.GetMinuteValue();
-        SnapsEngine.DisplayString(hourValue + ":" + minuteValue);
+
+        var time = true;
+
+
+        while (time)
+        {
+            int hourValue = SnapsEngine.GetHourValue();
+            int minuteValue = SnapsEngine.GetMinuteValue();
+            int secondValue = SnapsEngine.GetSecondsValue();
+            string currentTime = hourValue.ToString() + ":" + minuteValue.ToString() + ":" + secondValue.ToString();
+            SnapsEngine.Delay(1);
+            SnapsEngine.DisplayString(currentTime);
+        }
     }
 }
