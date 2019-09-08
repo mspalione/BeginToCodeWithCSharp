26a45fa34e017854e48777234e2ed0241337d543
Melissa Spalione <melissaspalione86@gmail.com>
2019-09-08 18:33:20 -0400

    Time to get up

---
+++ BeginToCodeWithCSharp/My Snaps apps/TimeToGetUp.cs
@@ -0,0 +1,14 @@
+﻿using SnapsLibrary;
+
+class TimeToGetUp
+{
+    public void StartProgram()
+    {
+        int hourValue = SnapsEngine.GetHourValue();
+        int minuteValue = SnapsEngine.GetMinuteValue();
+        if (hourValue >= 19 && minuteValue >= 15)
+            SnapsEngine.DisplayString("It is time to get up");
+        else
+            SnapsEngine.DisplayString("Go to bed");
+    }
+}
